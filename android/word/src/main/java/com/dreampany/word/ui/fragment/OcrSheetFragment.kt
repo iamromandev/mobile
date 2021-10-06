f2aff50974910baa2068af13cb8d2ce3cd5fc8d9
roman <roman@mac-pro.local>
2021-10-06 23:48:23 +0600

    update camera libraries

---
+++ android/word/src/main/java/com/dreampany/word/ui/fragment/OcrSheetFragment.kt
@@ -0,0 +1,10 @@
+package com.dreampany.word.ui.fragment
+
+/**
+ * Created by roman on 10/6/21
+ * Copyright (c) 2021 epany. All rights reserved.
+ * ifte.net@gmail.com
+ * Last modified $file.lastModified
+ */
+class OcrSheetFragment {
+}
